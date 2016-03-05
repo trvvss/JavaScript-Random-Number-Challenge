36fa21ef41dd7ca95c5bb94800ab506564e0a77c
Travis Nelson <travis.nelson.ca.us@gmail.com>
2016-03-05 12:45:20 +0900

    Solution

--- random.js
+++ random.js
@@ -1,1 +1,27 @@
-Math.floor(Math.random() * (6 - 1 + 1)) + 1; 
+var x = parseInt(prompt("First number please"));
+var y = parseInt(prompt("Second number please"));
+
+if (x > y) {
+	var upper = x;
+	var lower = y;
+} else if (y > x) {
+	var upper = y;
+	var lower = x;
+} else {
+	alert("Please refresh page and input two different numbers.")
+}
+
+console.log(x, y, upper, lower);
+
+function makeRando(lower, upper){
+	
+	rando = Math.floor(Math.random() * (upper-lower+ lower)) + 1; 
+	return rando;
+}
+
+makeRando(lower, upper);
+
+console.log(x, y, upper, lower, rando);
+
+
+document.write("A random number between "+ lower +" and "+ upper + " is " + rando);
